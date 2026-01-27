f19e22171251cbb2d4c7ee9eb0b19344e3fd6f9b
Волчугов Антон <toni.volchugov@mail.ru>
2026-01-27 18:24:00 +0300

    fix: fixed type 'addItem'

--- src/store/reducers/cartReducer.ts
+++ src/store/reducers/cartReducer.ts
@@ -1,38 +1,42 @@
-import type { CartAction, CartState } from '../types'
+import type { CartAction, CartState } from "../types";
 
 export function cartReducer(
-	state: CartState[],
-	action: CartAction,
+  state: CartState[],
+  action: CartAction,
 ): CartState[] {
-	const { type, payload } = action
+  const { type, payload } = action;
 
-	switch (type) {
-		case 'cart/addItem': {
-			const existItem = state.find(({ id }) => id === payload.id)
+  switch (type) {
+    case "cart/addItem": {
+      const existItem = state.find(({ id }) => id === payload.id);
 
-			if (existItem) {
-				return [...state, { ...existItem, count: existItem.count + 1 }]
-			} else {
-				return [...state, { ...payload, count: 1 }]
-			}
-		}
+      if (existItem) {
+        return state.map((el) =>
+          el.id === payload.id
+            ? { ...existItem, count: existItem.count + 1 }
+            : el,
+        );
+      } else {
+        return [...state, { ...payload, count: 1 }];
+      }
+    }
 
-		case 'cart/removeItem': {
-			return state.map(el =>
-				el.id === payload && el.count > 0 ? { ...el, count: el.count - 1 } : el,
-			)
-		}
+    case "cart/removeItem": {
+      return state.map((el) =>
+        el.id === payload && el.count > 0 ? { ...el, count: el.count - 1 } : el,
+      );
+    }
 
-		case 'cart/deleteItemCard': {
-			return state.filter(el => el.id !== payload)
-		}
+    case "cart/deleteItemCard": {
+      return state.filter((el) => el.id !== payload);
+    }
 
-		/* case 'cart/totalPrice': {
+    /* case 'cart/totalPrice': {
 			return state.reduce((sum, item) => sum + item.price * item.count, 0)
 		} */
 
-		default: {
-			return state
-		}
-	}
+    default: {
+      return state;
+    }
+  }
 }
